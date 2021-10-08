9e3084b366e7d62aa3428ab0fb46ed46f1ef9877
Maxwell0129 <90669912+Maxwell0129@users.noreply.github.com>
2021-10-08 22:28:17 +0800

    Change of Format

--- 2D Platformer Action Position Switcher, Activation and Deactivation Script.gd
+++ 2D Platformer Action Position Switcher, Activation and Deactivation Script.gd
@@ -4,14 +4,13 @@ export var right = 0
 export var left = 0
 
 func _physics_process(delta: float) -> void:
+	$Area2D/CollisionShape2D.disabled = true
 	if Input.is_action_pressed("ui_right"):
 		$Area2D.position = Vector2(right,0)
 	elif Input.is_action_pressed("ui_left"):
 		$Area2D.position = Vector2(left,0)
-
-	if Input.is_action_just_pressed("attack") and not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("use") and not Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left") :
 		$Area2D/CollisionShape2D.disabled = false
-	elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right"):
-		$Area2D/CollisionShape2D.disabled = true
+
 
 #Scene Setup: Position2D > Area2D > CollisionShape2D
