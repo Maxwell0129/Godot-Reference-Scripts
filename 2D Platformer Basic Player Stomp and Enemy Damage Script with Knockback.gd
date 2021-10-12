80665e10bc0b81bef4d327537cd2b3a3768be96e
Maxwell0129 <90669912+Maxwell0129@users.noreply.github.com>
2021-10-12 21:13:46 +0800

    Update 2D Platformer Basic Player Stomp and Enemy Damage Script with Knockback.gd
    Added parameters for y-axis knockback movement to compensate for being hit by floating enemy from above player

--- 2D Platformer Basic Player Stomp and Enemy Damage Script with Knockback.gd
+++ 2D Platformer Basic Player Stomp and Enemy Damage Script with Knockback.gd
@@ -7,11 +7,16 @@ func _on_PlayerHurtBox_area_entered(EnemyHurtArea: Area2D) -> void:
 	if $AnimatedSprite.global_position.x < EnemyHurtArea.global_position.x:
 		velocity.x -= KNOCKBACK
 		Input.action_press("ui_left")
+		Input.action_release("ui_left")
 	elif $AnimatedSprite.global_position.x > EnemyHurtArea.global_position.x:
 		velocity.x += KNOCKBACK
 		Input.action_press("ui_right")
-	Input.action_release("ui_left")
-	Input.action_release("ui_right")
+		Input.action_release("ui_right")
+	
+	if $AnimatedSprite.global_position.y < EnemyHurtArea.global_position.y:
+		velocity.y = (JUMP_FORCE * 0.5)
+	elif $AnimatedSprite.global_position.y > EnemyHurtArea.global_position.y:
+		velocity.y = -(JUMP_FORCE * 0.5)
 
 func _on_PlayerStompArea_area_entered(EnemyHitBox: Area2D) -> void:
 	velocity.y = JUMP_FORCE * 0.3
