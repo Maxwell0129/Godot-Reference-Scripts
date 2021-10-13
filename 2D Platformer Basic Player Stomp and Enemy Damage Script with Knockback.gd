92ee08c89fa8565b87e3535abb9238b1a4109752
Maxwell0129 <90669912+Maxwell0129@users.noreply.github.com>
2021-10-13 22:00:45 +0800

    Update 2D Platformer Basic Player Stomp and Enemy Damage Script with Knockback.gd
    Added velocity variable in scope

--- 2D Platformer Basic Player Stomp and Enemy Damage Script with Knockback.gd
+++ 2D Platformer Basic Player Stomp and Enemy Damage Script with Knockback.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 const KNOCKBACK = 500
 const JUMP_FORCE = -500
 
+var velocity = Vector2.ZERO
+
 func _on_PlayerHurtBox_area_entered(EnemyHurtArea: Area2D) -> void:
 	if $AnimatedSprite.global_position.x < EnemyHurtArea.global_position.x:
 		velocity.x -= KNOCKBACK
@@ -18,7 +20,7 @@ func _on_PlayerHurtBox_area_entered(EnemyHurtArea: Area2D) -> void:
 	elif $AnimatedSprite.global_position.y > EnemyHurtArea.global_position.y:
 		velocity.y = -(JUMP_FORCE * 0.5)
 
-func _on_PlayerStompArea_area_entered(EnemyHitBox: Area2D) -> void:
+func _on_PlayerStompArea_area_entered(_EnemyHitBox: Area2D) -> void:
 	velocity.y = JUMP_FORCE * 0.3
 
 #Signal 'area_entered' needs to be connected to work properly
